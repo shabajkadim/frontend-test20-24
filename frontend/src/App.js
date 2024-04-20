a08837c48caba550e33ce77202fd276f6b6d1869
Shahabaj kadim <shahabajkadim7@gmail.com>
2024-04-21 00:29:17 +0530

    updates

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchMovie from './components/SearchMovie';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchMovie/>
+    </div>
+  );
+}
+
+export default App;
